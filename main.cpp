84014331765728926d861012642196dae37fab39
Michał Trojak <32899559+michalt38@users.noreply.github.com>
2020-04-30 11:56:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-
+#include "game.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    cout << "Enter size of one dimension of the board: ";
+    int dimension;
+    cin >> dimension;
+    cout << "Enter your marker (X or O): ";
+    char marker;
+    cin >> marker;
+    while(!(marker == 'X' || marker == 'O'))
+    {
+        cout << "Wrong marker! Choose between X and O: ";
+        cin >> marker;
+    }
+    Marker userMarker = (Marker)marker;
+    Game game(dimension, userMarker);
+    game.play();
+    system("pause");
 }
